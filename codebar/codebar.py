b08f07055910d84cda53cedc5cab8cce7b0dd8b0
iDenisman <145935949+DenisKopasek@users.noreply.github.com>
2024-05-16 16:45:52 +0200

    Language_learning_app.codebar
    Step by step language learning application. With functional word guessing and correction.

---
+++ codebar/codebar.py
@@ -0,0 +1,52 @@
+import tkinter as tk
+import random
+
+vocabulary = {
+    "bonita": "nice",
+    "chaqueta": "jacket",
+}
+vocabulary["prima"]="cousin"
+
+root = tk.Tk()
+root.title("My Application")
+
+prompt_text = tk.StringVar()
+userInput = tk.StringVar()
+feedback_text = tk.StringVar()
+
+def next():
+    p = prompt_text.get()
+    # if p != "":
+    #     o = vocabulary[p]
+    #     if o == userInput.get():
+    #         feedback_text.set("Correct")
+    #     else:
+    #         feedback_text.set(f"Incorrect : {o}")
+    res = random.choice(list(vocabulary.items()))
+    prompt_text.set(res[0])
+    root.update_idletasks()
+
+
+# def restart():
+
+
+
+
+
+
+
+
+prompt = tk.Label(root,textvariable=prompt_text)
+prompt.pack()
+feedback = tk.Label(root, textvariable=feedback_text)
+feedback.pack()
+input = tk.Entry(root,textvariable=userInput)
+input.pack()
+button = tk.Button(root,text="Next", command=next)
+button.pack()
+# button = tk.Button(root,text="Restart", command=restart)
+# button.pack()
+
+root.mainloop()
+
+
